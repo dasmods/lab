b52c0e8a21b4f359368a437bf0117cce63e9327c
Jared <jared@jaredjohnson.dev>
2021-06-22 22:40:21 -0400

    Continue progress on LabVector2

--- src/shared/LabVector2.ts
+++ src/shared/LabVector2.ts
@@ -1,9 +1,12 @@
-import { t } from "@rbxts/t";
-
 /* eslint-disable @typescript-eslint/no-this-alias */
 export class LabVector2 {
-	x: number;
-	y: number;
+	static fromRbx(vector: Vector2): LabVector2 {
+		const v = vector;
+		return new LabVector2(v.X, v.Y);
+	}
+
+	readonly x: number;
+	readonly y: number;
 
 	constructor(x: number, y: number) {
 		this.x = x;
@@ -12,6 +15,12 @@ export class LabVector2 {
 
 	toRbx(): Vector2 {
 		return new Vector2(this.x, this.y);
+	}
+
+	eq(vector: LabVector2): boolean {
+		const v1 = this;
+		const v2 = vector;
+		return v1.x === v2.x && v1.y === v2.y;
 	}
 
 	add(vector: LabVector2): LabVector2 {
@@ -34,15 +43,12 @@ export class LabVector2 {
 		return new LabVector2(x, y);
 	}
 
-	mul(value: LabVector2 | number): LabVector2 {
-		return t.number(value) ? this.scalarProduct(value) : this.dotProduct(value);
-	}
+	mul(vector: LabVector2): LabVector2 {
+		const v1 = this;
+		const v2 = vector;
 
-	private scalarProduct(scalar: number): LabVector2 {
-		const v = this;
-
-		const x = v.x * scalar;
-		const y = v.y * scalar;
+		const x = v1.x * v2.x;
+		const y = v1.y * v2.y;
 
 		return new LabVector2(x, y);
 	}
@@ -52,10 +58,20 @@ export class LabVector2 {
 	 * the project of the second onto the first.
 	 * https://physics.info/vector-multiplication/
 	 */
-	private dotProduct(vector: LabVector2): LabVector2 {
+	dot(vector: LabVector2): number {
 		const v1 = this;
 		const v2 = vector;
-		return this;
+		return v1.x * v2.x + v1.y * v2.y;
+	}
+
+	cross(vector: LabVector2): number {
+		const v1 = this;
+		const v2 = vector;
+		// Cross product really returns a vector, but
+		// multiplied by k unit vector. This can't be
+		// described in 2d, so Roblox returns a number.
+		// https://developer.roblox.com/en-us/api-reference/datatype/Vector2
+		return v1.x * v2.y - v1.y * v2.x;
 	}
 
 	magnitude(): number {
--- src/shared/tests/LabVector2.spec.ts
+++ src/shared/tests/LabVector2.spec.ts
@@ -1,7 +1,102 @@
 /// <reference types="@rbxts/testez/globals" />
 
+import { LabVector2 } from "shared/LabVector2";
+
 export = () => {
-	it("succeeds", () => {
-		print("hi");
+	describe("constructor", () => {
+		it("creates a vector with x and y", () => {
+			const v = new LabVector2(1, 2);
+
+			expect(v.x).equal(1);
+			expect(v.y).equal(2);
+		});
+	});
+
+	describe("toRbx", () => {
+		it("creates a Vector2 instance", () => {
+			const lv = new LabVector2(1, 1);
+			const rv = lv.toRbx();
+
+			expect(lv.x).equal(rv.X);
+			expect(lv.y).equal(rv.Y);
+		});
+	});
+
+	describe("add", () => {
+		it("adds two vectors", () => {
+			const v1 = new LabVector2(1, 1);
+			const v2 = new LabVector2(2, 3);
+
+			const v3 = v1.add(v2);
+
+			expect(v3.x).equal(3);
+			expect(v3.y).equal(4);
+		});
+
+		it("mimics rbx", () => {
+			const lv1 = new LabVector2(4, 4);
+			const lv2 = new LabVector2(1, 2);
+			const lv3 = lv1.add(lv2);
+
+			const rv1 = new Vector2(4, 4);
+			const rv2 = new Vector2(1, 2);
+			const rv3 = rv1.add(rv2);
+
+			expect(lv3.x).equal(rv3.X);
+			expect(lv3.y).equal(rv3.Y);
+		});
+	});
+
+	describe("mul", () => {
+		it("multiplies vectors", () => {
+			const v1 = new LabVector2(1, 2);
+			const v2 = new LabVector2(2, 3);
+
+			const v3 = v1.mul(v2);
+
+			expect(v3.x).equal(2);
+			expect(v3.y).equal(6);
+		});
+
+		it("mimics rbx", () => {
+			const l1 = new LabVector2(1, 2);
+			const l2 = new LabVector2(2, 3);
+			const l3 = l1.mul(l2);
+
+			const r1 = new Vector2(1, 2);
+			const r2 = new Vector2(2, 3);
+			const r3 = r1.mul(r2);
+
+			expect(l3.x).equal(r3.X);
+			expect(l3.y).equal(r3.Y);
+		});
+	});
+
+	describe("dot", () => {
+		it("mimics rbx", () => {
+			const l1 = new LabVector2(1, 2);
+			const l2 = new LabVector2(2, 3);
+			const lDotProduct = l1.dot(l2);
+
+			const r1 = new Vector2(1, 2);
+			const r2 = new Vector2(2, 3);
+			const rDotProduct = r1.Dot(r2);
+
+			expect(lDotProduct).equal(rDotProduct);
+		});
+	});
+
+	describe("cross", () => {
+		it("mimics rbx", () => {
+			const l1 = new LabVector2(1, 2);
+			const l2 = new LabVector2(2, 3);
+			const lCrossProduct = l1.cross(l2);
+
+			const r1 = new Vector2(1, 2);
+			const r2 = new Vector2(2, 3);
+			const rCrossProduct = r1.Cross(r2);
+
+			expect(lCrossProduct).equal(rCrossProduct);
+		});
 	});
 };
