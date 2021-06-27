8be5f4ba64b83e80aca27a91d8eac6e77ab6266f
Jared <jared@jaredjohnson.dev>
2021-06-27 15:51:36 -0400

    Render Vector3s

---
+++ src/server/Vector3.server.ts
@@ -0,0 +1,34 @@
+import { Colors, render, RenderType } from "shared/rendering";
+
+const createRenderGroup = (offset: Vector3) => (text: string, vector3: Vector3, color: Color3) => {
+	render({ type: RenderType.Vector3, text, offset, vector3, color });
+};
+
+const renderGroupFactory = (initialOffset: Vector3, deltaOffset: Vector3) => {
+	let offset = initialOffset;
+	return () => {
+		const currOffset = offset;
+		offset = offset.add(deltaOffset);
+		return createRenderGroup(currOffset);
+	};
+};
+
+const nextRenderGroup = renderGroupFactory(new Vector3(0, 5, 10), new Vector3(10, 0, 0));
+
+const A = new Vector3(1, 5, 4);
+const B = new Vector3(3, -2, 3);
+
+const r1 = nextRenderGroup();
+r1("A", A, Colors.RED);
+r1("B", B, Colors.BLUE);
+r1("A+B", A.add(B), Colors.GREEN);
+
+const r2 = nextRenderGroup();
+r2("A", A, Colors.RED);
+r2("B", B, Colors.BLUE);
+r2("-B", B.mul(-1), new Color3(0, 1, 0.87));
+r2("A-B", A.sub(B), Colors.GREEN);
+
+const r3 = nextRenderGroup();
+r3("A", A, Colors.WHITE);
+r3("A unit", A.Unit, Colors.RED);
--- src/shared/rendering.ts
+++ src/shared/rendering.ts
@@ -2,6 +2,7 @@ import { Workspace } from "@rbxts/services";
 
 export enum RenderType {
 	Vector2 = "Vector2",
+	Vector3 = "Vector3",
 }
 
 type Vector2Params = {
@@ -12,7 +13,15 @@ type Vector2Params = {
 	text: string;
 };
 
-export type RenderParams = Vector2Params;
+type Vector3Params = {
+	type: RenderType.Vector3;
+	vector3: Vector3;
+	offset: Vector3;
+	color: Color3;
+	text: string;
+};
+
+export type RenderParams = Vector2Params | Vector3Params;
 
 export const Colors = {
 	RED: new Color3(1, 0, 0),
@@ -28,8 +37,11 @@ export const render = (params: RenderParams) => {
 		case RenderType.Vector2:
 			part = createVector2Part(params);
 			break;
+		case RenderType.Vector3:
+			part = createVector3Part(params);
+			break;
 		default:
-			error(`unexpected type: ${params.type}`);
+			error(`unexpected params: ${params}`);
 	}
 
 	part.Parent = Workspace;
@@ -49,6 +61,36 @@ const createVector2Part = (params: Vector2Params) => {
 	const magnitude = vector2.Magnitude;
 	const p1 = offset;
 	const p2 = new Vector3(vector2.X, vector2.Y, 0).add(offset);
+
+	const part = createPart();
+	part.Color = color;
+	part.Size = new Vector3(0.1, 0.1, magnitude);
+	part.CFrame = CFrame.lookAt(p1, p2).ToWorldSpace(new CFrame(0, 0, -magnitude / 2));
+
+	const billboard = new Instance("BillboardGui", part);
+	billboard.Adornee = part;
+	billboard.Enabled = true;
+	billboard.Size = new UDim2(1, 5, 1, 1);
+	billboard.AlwaysOnTop = true;
+
+	const textLabel = new Instance("TextLabel", billboard);
+	textLabel.Text = text;
+	textLabel.Visible = true;
+	textLabel.Size = new UDim2(1, 5, 1, 1);
+	textLabel.BackgroundTransparency = 1;
+	textLabel.TextColor3 = color;
+	textLabel.TextXAlignment = Enum.TextXAlignment.Right;
+	textLabel.TextSize = 20;
+
+	return part;
+};
+
+const createVector3Part = (params: Vector3Params) => {
+	const { vector3, offset, color, text } = params;
+
+	const magnitude = vector3.Magnitude;
+	const p1 = offset;
+	const p2 = new Vector3(vector3.X, vector3.Y, vector3.Z).add(offset);
 
 	const part = createPart();
 	part.Color = color;
