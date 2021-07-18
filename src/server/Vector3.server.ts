07b1c0d37d67ef3f253bcef4a73e583e84451cef
Jared <jared@jaredjohnson.dev>
2021-07-18 15:58:11 -0400

    Finish Vector3 renderings

--- src/server/Vector3.server.ts
+++ src/server/Vector3.server.ts
@@ -16,7 +16,7 @@ const renderGroupFactory = (initialOffset: Vector3, deltaOffset: Vector3) => {
 const nextRenderGroup = renderGroupFactory(new Vector3(0, 5, 10), new Vector3(10, 0, 0));
 
 const A = new Vector3(1, 1, 1);
-const B = new Vector3(1, -1, 1);
+const B = new Vector3(2, -3, 4);
 
 const r1 = nextRenderGroup();
 r1("A", A, Colors.RED);
@@ -37,3 +37,10 @@ const r4 = nextRenderGroup();
 r4("A", A, Colors.RED);
 r4("B", B, Colors.BLUE);
 r4("A.Cross(B)", A.Cross(B), Colors.WHITE);
+
+const r5 = nextRenderGroup();
+r5("A", A, Colors.RED);
+r5("B", B, Colors.BLUE);
+r5("A.Lerp(B, 0.1)", A.Lerp(B, 0.1), Colors.GREEN);
+r5("A.Lerp(B, 0.5)", A.Lerp(B, 0.5), Colors.GREEN);
+r5("A.Lerp(B, 1.5)", A.Lerp(B, 1.5), Colors.GREEN);
